5d19b9e61336cb5c79ae3dfb936a27b16c5f73b0
Yash Dighe <yash.r.dighe@gmail.com>
2020-09-20 13:08:41 -0700

    data type file

---
+++ dataTypes.py
@@ -0,0 +1,19 @@
+list = ["apples", "bananas", "cabbages", "7 oranges", "2 tomtoes"]
+print(list)
+
+dic = {"rajesh": 51, "yash": 14, "rajesh": 45}
+print(dic)
+
+
+str = "phone number"
+print(str)
+
+tuple = (2, "mom", "dad", 2, "yash")
+print(tuple)
+
+set = {"computer", "monitor", "mouse", "mouse", "keyboard"}
+print(set)
+
+for seq in range(-5, 0):
+    print(seq)
+
